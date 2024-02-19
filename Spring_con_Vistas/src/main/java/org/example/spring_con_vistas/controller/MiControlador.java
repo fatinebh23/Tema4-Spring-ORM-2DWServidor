e31c63161b1a31aaf33c178b50c8aeab0537509b
daw2 <fatinebh23@gmail.com>
2024-02-19 13:54:34 +0100

    pagina 15

--- Spring_con_Vistas/src/main/java/org/example/spring_con_vistas/controller/MiControlador.java
+++ Spring_con_Vistas/src/main/java/org/example/spring_con_vistas/controller/MiControlador.java
@@ -7,13 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @Controller
 public class MiControlador {
