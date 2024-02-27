bb17e60813d473c156c55e66e3f29dcb40237dff
Fatine Bouhrich <fatinebh23@gmail.com>
2024-02-27 20:53:32 +0100

    estructura de examen

---
+++ examen_final/src/main/java/org/example/examen_final/ExamenFinalApplication.java
@@ -0,0 +1,13 @@
+package org.example.examen_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalApplication.class, args);
+	}
+
+}
---
+++ examen_final/src/test/java/org/example/examen_final/ExamenFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.examen_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
